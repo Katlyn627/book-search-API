47ea78d58eceec68f44491376e437fc0a6efa06c
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-11 10:59:23 -0700

    fixed queries and deleted API file

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,20 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
-    {
-        me {
-            _id
-            username
-            email
-        }
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
